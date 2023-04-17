42df55a2752986a7e7b963300f0c489323708234
ababoum <ababou237@hotmail.com>
2023-04-17 16:40:50 +0200

    add faulty examples + redirect output in file

--- src/avaj_launcher/simulator/Logger.java
+++ src/avaj_launcher/simulator/Logger.java
@@ -11,6 +11,11 @@ public class Logger {
 
     static public void turnOn(String output_filename) throws IOException {
         writer = new FileWriter(output_filename);
+        try {
+            writer.write("");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     static public void turnOff() {
@@ -29,80 +34,98 @@ public class Logger {
     }
 
     static public void towerAnnouncesRegistration(Flyable registree) {
-        System.out.println("Tower says: " + flyableLabel(registree) + " registered to weather tower.");
+        try {
+            writer.append("Tower says: " + flyableLabel(registree) + " registered to weather tower.\n");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     static public void towerAnnouncesUnregistration(Flyable registree) {
-        System.out.println("Tower says: " + flyableLabel(registree) + " unregistered from weather tower.");
+        try {
+            writer.append("Tower says: " + flyableLabel(registree) + " unregistered from weather tower.\n");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     static public void update(Flyable broadcaster, String action) {
+        try {
 
-        System.out.println(flyableLabel(broadcaster) + " " + action + ".");
+            writer.append(flyableLabel(broadcaster) + " " + action + ".\n");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 
     static public void weatherReact(Flyable broadcaster, String weather) {
         String prompt = flyableLabel(broadcaster) + ": ";
-        switch (broadcaster.getType()) {
-            case "Baloon":
-                switch (weather) {
-                    case "SUN":
-                        System.out.println(prompt + "Let's enjoy the good weather and take some pics.");
-                        break;
-                    case "RAIN":
-                        System.out.println(prompt + "Damn you rain! You messed up my baloon.");
-                        break;
-                    case "SNOW":
-                        System.out.println(prompt + "It's snowing. We're gonna crash.");
-                        break;
-                    case "FOG":
-                        System.out.println(prompt + "Fog messes with my head.");
-                        break;
-                    default:
-                        break;
-                }
-                break;
+        try {
+            switch (broadcaster.getType()) {
+                case "Baloon":
+                    switch (weather) {
+                        case "SUN":
+                            writer.append(prompt + "Let's enjoy the good weather and take some pics.");
+                            break;
+                        case "RAIN":
+                            writer.append(prompt + "Damn you rain! You messed up my baloon.");
+                            break;
+                        case "SNOW":
+                            writer.append(prompt + "It's snowing. We're gonna crash.");
+                            break;
+                        case "FOG":
+                            writer.append(prompt + "Fog messes with my head.");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
 
-            case "JetPlane":
-                switch (weather) {
-                    case "SUN":
-                        System.out.println(prompt + "What a wonderful weather.");
-                        break;
-                    case "RAIN":
-                        System.out.println(prompt + "It's raining. Better watch out for lightings.");
-                        break;
-                    case "SNOW":
-                        System.out.println(prompt + "OMG! Winter is coming!");
-                        break;
-                    case "FOG":
-                        System.out.println(prompt + "Can I see? Barely!");
-                        break;
-                    default:
-                        break;
-                }
-                break;
+                case "JetPlane":
+                    switch (weather) {
+                        case "SUN":
+                            writer.append(prompt + "What a wonderful weather.");
+                            break;
+                        case "RAIN":
+                            writer.append(prompt + "It's raining. Better watch out for lightings.");
+                            break;
+                        case "SNOW":
+                            writer.append(prompt + "OMG! Winter is coming!");
+                            break;
+                        case "FOG":
+                            writer.append(prompt + "Can I see? Barely!");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
 
-            case "Helicopter":
-                switch (weather) {
-                    case "SUN":
-                        System.out.println(prompt + "This is hot.");
-                        break;
-                    case "RAIN":
-                        System.out.println(prompt + "Chop-chop that rain!");
-                        break;
-                    case "SNOW":
-                        System.out.println(prompt + "My rotor is going to freeze!");
-                        break;
-                    case "FOG":
-                        System.out.println(prompt + "I hope this is all a dream.");
-                        break;
-                    default:
-                        break;
-                }
-                break;
+                case "Helicopter":
+                    switch (weather) {
+                        case "SUN":
+                            writer.append(prompt + "This is hot.");
+                            break;
+                        case "RAIN":
+                            writer.append(prompt + "Chop-chop that rain!");
+                            break;
+                        case "SNOW":
+                            writer.append(prompt + "My rotor is going to freeze!");
+                            break;
+                        case "FOG":
+                            writer.append(prompt + "I hope this is all a dream.");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
 
-            default:
-                break;
+                default:
+                    break;
+            }
+            writer.append("\n");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
